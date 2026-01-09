cf7bea2ccac2d04f9dcf0713321764eafa635b0c
Dmytro Kyselov <dima1989k@ukr.net>
2026-01-09 20:13:46 +0200

    add mode in validation

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -31,6 +31,7 @@ export default function Login() {
 
   const form = useForm<LoginFormValues>({
     resolver: zodResolver(loginSchema),
+    mode: 'onChange',
     defaultValues: {
       email: '',
       password: '',
