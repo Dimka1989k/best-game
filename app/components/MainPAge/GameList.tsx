569d87d72feb88500478d59d3867cfd5d5801913
Dmytro Kyselov <dima1989k@ukr.net>
2026-01-14 20:32:39 +0200

    add formatting

--- app/components/MainPAge/GameList.tsx
+++ app/components/MainPAge/GameList.tsx
@@ -10,34 +10,18 @@ export default function GameList() {
       {GAME_LIST.map((game) => (
         <Card
           key={game.id}
-          className="w-full max-w-74 h-90.5 max-sm:max-h-57.5 object-contain bg-center flex flex-col p-6
-                     max-md:pt-2.5 max-md:pb-4 max-md:px-2.5
-                     radius-lg shadow-cards-games"
+          className="w-full max-w-74 h-90.5 max-sm:max-h-57.5 object-contain bg-center flex flex-col p-6 max-md:pt-2.5 max-md:pb-4 max-md:px-2.5 radius-lg shadow-cards-games"
           style={{ backgroundImage: `url(${game.background})` }}
         >
           <CardHeader>
             <div
-              className={`shadow-green flex items-center justify-center
-                          bg-[linear-gradient(116.24deg,#B9FF58_-6.88%,#69B400_83.61%)]
-                          ${game.badgeWidthClass}
-                          max-sm:h-6.5 h-7 radius-pill
-                          text-white text-inter-bold max-md:text-sm!`}
+              className={`shadow-green flex items-center justify-center bg-[linear-gradient(116.24deg,#B9FF58_-6.88%,#69B400_83.61%)] ${game.badgeWidthClass} max-sm:h-6.5 h-7 radius-pill text-white text-inter-bold max-md:text-sm!`}
             >
               {game.badge}
             </div>
           </CardHeader>
           <CardContent className="relative flex flex-col justify-center items-center text-center">
-            <div
-              className="
-      absolute inset-0
-      translate-y-[10%]
-      bg-[#FFFFFF03]
-      backdrop-blur-lg
-      pointer-events-none
-      mask-[linear-gradient(to_bottom,transparent_47.37%,black_100%)]
-      [-webkit-mask-image:linear-gradient(to_bottom,transparent_47.37%,black_100%)] rounded-b-lg
-    "
-            />
+            <div className="absolute inset-0 translate-y-[10%] bg-[#FFFFFF03] backdrop-blur-lg pointer-events-none mask-[linear-gradient(to_bottom,transparent_47.37%,black_100%)] [-webkit-mask-image:linear-gradient(to_bottom,transparent_47.37%,black_100%)] rounded-b-lg" />
             <div className="relative z-10 ">
               <p className="max-sm:mt-9 mt-29 text-satoshi text-white max-sm:text-xl!">
                 {game.title}
